c05ef5f08390be9fca3ebce7b13c4c3be3421e8c
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-06 14:20:05 +0530

    14

---
+++ 14_Project/Movieapp/urls.py
@@ -0,0 +1,39 @@
+from django.contrib import admin
+from django.urls import path,include
+from . views import *
+
+
+##### FOR ROUTERS
+from rest_framework import viewsets
+
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter() 
+router.register('generic_stream',          RouterStream , basename='streamplatform')
+router.register('modelviewset_watchlist',  Watchlist_MVS, basename='watchlist')
+router.register('GV_reviews', Reviews_Views, basename='review')
+
+urlpatterns = [
+   path('',include(router.urls)),
+
+   path('reviews/',                    ReviewAll.as_view()) ,
+   path('reviews/<int:pk>/',           ReviewbyId.as_view()) ,
+   ## 1 User can Put 1 review Only
+   path('add_reviews/<int:pk>/',       AddNewReviewtoWatchlist.as_view()) ,
+]
+
+
+""" 
+
+#### GENERICVIEWSET
+http://127.0.0.1:8000/movieapi/generic_stream/
+http://127.0.0.1:8000/movieapi/generic_stream/2/
+
+#### MODEL VIEWSET
+http://127.0.0.1:8000/movieapi/modelviewset_watchlist/
+http://127.0.0.1:8000/movieapi/modelviewset_watchlist/1/
+
+#### MODELREAADONLY DATASET
+http://127.0.0.1:8000/movieapi/modelreadviewset_review/
+http://127.0.0.1:8000/movieapi/modelreadviewset_review/1/
+
+"""
---
+++ 14_Project/readme.txt
@@ -0,0 +1,11 @@
+Class Based RestApi using Serializers --> with Validation
+1.Here for serialization is Done using Modelserializer Class used.
+2.Views wriiten on class Based.
+3.It works with 4 main methods(get,post,put,patch,delete).
+4.Database used is MySQL.
+5.You can create superuser --> To insert fields.
+6.Here two tables are created and the linked with Foreignkey Relation.
+7.Now Here restriction of review by user going to done.
+  Reviews Table--> Plan to written in GenericViewset
+  One User can Post Only one review on a movie.
+  Reviewer Name Can be seen --> Using Django User Class in Foreignkey
