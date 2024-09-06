0117e8a4f038794335f99b0bd42249061137058f
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-06 16:45:08 +0530

    26 Project Creation

---
+++ 26_Project/Movieapp/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.core.validators import MaxValueValidator,MinValueValidator  ## For 5Star Field
+
+### User From Django -->
+from django.contrib.auth.models import User
+
+class MyStreamPlatform(models.Model):
+    name    = models.CharField(max_length=30)
+    about   = models.CharField(max_length=150)
+    website = models.URLField(max_length=100)
+
+   
+    def __str__(self):
+        return self.name
+
+
+class MyWatchlist(models.Model):
+    title         = models.CharField(max_length=50)
+    storyline     = models.CharField(max_length=200)
+    platform      = models.ForeignKey(MyStreamPlatform , on_delete=models.CASCADE , related_name= "watchdetails")
+    active        = models.BooleanField(default=True)
+    created       = models.DateField(auto_now_add=True)
+    avg_rating    = models.FloatField(default=0)
+    number_rating = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+
+class MyReview(models.Model):
+    reviewer_name = models.ForeignKey(User , on_delete=models.CASCADE)  # This will show the Superuser,staff in Django
+    rating      = models.PositiveBigIntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    description = models.CharField(max_length=200,null=True)
+    watchlist   = models.ForeignKey(MyWatchlist,on_delete=models.CASCADE,related_name='reviewinfo')
+    active      = models.BooleanField(default=True)
+    created     = models.DateTimeField(auto_now_add=True)
+    update      = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f" {self.watchlist.title}  {self.rating} star | {self.reviewer_name} "
+        
+
---
+++ 26_Project/Movieapp/urls.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from django.urls import path,include
+from . views import *
+
+
+
+
+
+urlpatterns = [
+   ###  REVIEWS --> Create,Update,View All,Detail Id View
+   path('movie/showall_reviews/',            ReviewAll.as_view()),
+   path('movie/<int:pk>/review_create/',     AddNewReviewtoWatchlist.as_view()),
+   path('movie/<int:pk>/review_detail/',     ReviewbyId.as_view()),
+   # path('movie/<int:pk>/review_update/',  UpdateReviewView.as_view()),
+
+
+   ### STREAMS --> Create,Viewall,Detail ID View,Delete
+   path('stream/showall_createstream/',              StreamPlatform.as_view()),
+   path('stream/<int:id>/detailview_updatestream/',  StreamPlatformbyID.as_view()),
+
+
+
+   ### WATCHLIST -->
+   path('watchlist/showall_createwatchlist/',             Watchlist.as_view()) ,
+   path('watchlist/<int:id>/detailview_updatewatchlist/', Watchlistbyid.as_view()) ,
+
+
+   ### FILTER LEARN ---> http://127.0.0.1:8000/movieapi/reviews/siva/
+   path('reviews/<str:username>/', UserReviewsfilter.as_view()) ,
+
+
+]
+
+"""
+Detailed Url/Method Information are created and provided in Excel Sheet.
+Please Follow the Excel Sheet.
+"""
+
+
+
+
+
+
+
