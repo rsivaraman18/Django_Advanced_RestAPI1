8a995518438db3d6f7e22f31dac5a8dd0a42d08e
rsivaraman18 <rsivaraman18@gmail.com>
2024-09-11 15:01:02 +0530

    Updated  Testcase-2

--- 29_Project/UserProfilesApp/tests.py
+++ 29_Project/UserProfilesApp/tests.py
@@ -32,3 +32,9 @@ class LoginLogoutTestCase(APITestCase):
                 }
         response = self.client.post(reverse('UserLogin'),data)
         self.assertEqual(response.status_code,status.HTTP_200_OK)
+
+    def test_logout(self):
+        self.token = Token.objects.get(user__username = "example")
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        response = self.client.post(reverse('UserLogout'))
+        self.assertEqual(response.status_code,status.HTTP_200_OK)
